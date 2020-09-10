c5c0f80502050f243d2176e10833344131920239
adib <dib.firman@gmail.com>
2020-09-10 23:00:51 +0700

    chore: prepare ui detail ;

--- src/pages/Detail/component/index.tsx
+++ src/pages/Detail/component/index.tsx
@@ -34,15 +34,41 @@ export default function DetailPage(
   if (isLoading) return <>Loading...</>;
   else
     return (
-      <div>
-        <button onClick={() => props.history.push("/")}>go back</button>
+      <div className="p-4">
+        <div className="grid grid-flow-col justify-between">
+          <button
+            onClick={() => props.history.push("/")}
+            className="flex items-center bg-transparent text-mycolor-90"
+          >
+            <svg
+              className="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+            <span>Back</span>
+          </button>
+          <button
+            onClick={catchPoke}
+            className="bg-mycolor-60 text-white font-bold py-2 px-4 rounded"
+          >
+            Catch Me
+          </button>
+        </div>
         <img
-          width="100px"
-          height="100px"
+          width="55%"
+          className="m-auto object-contain"
           alt={result?.name}
           src={getImgPokemon(result?.id)}
         />
-        <button onClick={catchPoke}>catch me</button>
       </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 const THEME_CUSTOM = {
+  "mycolor-10": "#F4E1F2",
+  "mycolor-20": "#EECCED",
   "mycolor-50": "#765BAA",
   "mycolor-60": "#594E93",
   "mycolor-80": "#3F455D",
+  "mycolor-90": "#8962B6",
   "mycolor-100": "#181B1D",
+  "mycolor-120": "#5B4C95",
   "mycolor-150": "#44486D",
 };
 
@@ -12,6 +16,7 @@ module.exports = {
       ...theme("colors"),
       ...THEME_CUSTOM,
     }),
+    borderColor: { ...THEME_CUSTOM },
     textColor: { ...THEME_CUSTOM },
   },
 };
